cd4af4025c9647795bb7b10d5ebdcc93522f2301
Jasper <147628504+Jazzknow@users.noreply.github.com>
2024-09-17 22:57:30 +0800

    Add files via upload

---
+++ landing.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CvSU-CCAT Hostel</title>
+    <link rel="stylesheet" href="../website/landing.css">
+    <link rel="icon" href="../assets/img/logo_tab.png" type="x-icon">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.6.0/css/fontawesome.min.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3S10sibVcOQVnN" crossorigin="anonymous">
+    </link>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn">
+            <i class="fa-solid fa-bars"></i>
+        </label>
+        <a href="landing.php"><img src="../assets/img/navlogo.png" alt="Logo" class="logo"></a>
+        <ul>
+            <li><a class="active" href="#home">HOME</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#rooms">ROOMS</a></li>
+            <li><a href="#">FACILITIES</a></li>
+            <li><a href="#contact">CONTACT</a></li>
+            <li><a class="book-link" href="#">BOOK NOW</a></li>
+            <li><a href="../assets/user_login.php">LOGIN</a></li>
+        </ul>
+    </nav>
+
+    <section class="home" id="home">
+        <H1 class="homedescription"> A MODERN DAY HOTEL with <br> SCHOOL ELEGANCE IN HEART OF CAVITE </H1>
+    </section>
+
+    <section class="about" id="about">
+        <div class="about-content">
+            <div class="text-content">
+                <h1 class="aboutwelcome">WELCOME</h1>
+                <h1 class="abouttitle">ABOUT HOSTEL MANAGEMENT SYSTEM</h1>
+                <p class="aboutdescription">Welcome to the official page for the HOSTEL MANAGEMENT SYSTEM at Cavite
+                    State University CCAT Campus (CVSU CCAT). Our platform is designed to streamline and enhance the
+                    management of the hostel accommodations, ensuring a seamless experience for both students and
+                    administrators.</p>
+                <button class="booknow">BOOK NOW!</button>
+            </div>
+            <div class="hostelabout">
+                <video class="hostelvideo" autoplay muted loop>
+                    <source src="image/aboutvid.mp4">
+                </video>
+            </div>
+        </div>
+    </section>
+
+    <section class="rooms" id="rooms">
+        <div class="container">
+            <div class="slide">
+                <div class="item" style="background-image: url(image/standard1.jpg);">
+                    <div class="content">
+                        <div class="roomname"> Standard Room # </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/standard2.jpg);">
+                    <div class="content">
+                        <div class="roomname"> Standard Room #1 </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/standard3.png);">
+                    <div class="content">
+                        <div class="roomname"> Standard Room #2 </div>
+                        <div class="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/standard4.jpg);">
+                    <div class="content">
+                        <div class="roomname"> Standard Room #3 </div>
+                        <div class="roomdescription">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/vip1.jpg);">
+                    <div class="content">
+                        <div class="roomname"> Standard Room #4 </div>
+                        <div class="roomdescription">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/vip2.jpg);">
+                    <div class="content">
+                        <div class="roomname"> VIP Room #1 </div>
+                        <div class="roomdescription">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/vip3.jpg);">
+                    <div class="content">
+                        <div class="roomname"> VIP Room #2 </div>
+                        <div class="roomdescription">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item" style="background-image: url(image/vip4.jpg);">
+                    <div class="content">
+                        <div class="roomname"> VIP Room #3 </div>
+                        <div class="roomdescription">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus reprehenderit,
+                            nesciunt odit eius quod repellat beatae cum esse, vitae earum similique sequi harum?
+                            Architecto vero nisi itaque molestiae quasi?
+                        </div>
+                    </div>
+                </div>
+
+                <div class="roombuttons">
+                    <button class="prev"></button>
+                    <button class="next"></button>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+    <section class="contact" id="contact">
+        <div class="contact-content">
+            <h2>CONTACT US</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam vel ex quae et suscipit hic modi!
+                Repudiandae, ratione deserunt a quo necessitatibus voluptate dicta dolorem totam alias iure eaque
+                repellendus.</p>
+        </div>
+        <div class="contact-container">
+            <div class="contactInfo">
+                <div class="contact-box">
+                    <div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                    <div class="contact-text">
+                        <h3>Address</h3>
+                        <p>ROSARIO CAVITE</p>
+                    </div>
+                </div>
+                <div class="contact-box">
+                    <div class="icon"><i class="fa fa-phone" aria-hidden="true"></i></div>
+                    <div class="contact-text">
+                        <h3>Phone</h3>
+                        <p>ROSARIO CAVITE</p>
+                    </div>
+                </div>
+                <div class="contact-box">
+                    <div class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></div>
+                    <div class="contact-text">
+                        <h3>Email</h3>
+                        <p>ROSARIO CAVITE</p>
+                    </div>
+                </div>
+            </div>
+            <div class="contactForm">
+                <form>
+                    <h2>Send Message</h2>
+                    <div class="inputBox">
+                        <input type="text" name="" required="required">
+                        <span> Full Name </span>
+                    </div>
+                    <div class="inputBox">
+                        <input type="text" name="" required="required">
+                        <span> Email </span>
+                    </div>
+                    <div class="inputBox">
+                        <textarea required="required"></textarea>
+                        <span> Type your Message... </span>
+                    </div>
+                    <div class="inputBox">
+                        <input type="submit" name="" value="Send">
+                    </div>
+                </form>
+            </div>
+
+
+    </section>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
